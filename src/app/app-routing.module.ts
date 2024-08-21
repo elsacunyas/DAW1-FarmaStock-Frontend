37a28b9dc83f299c476800d1b0972774867bc094
Ruben <angelocana44@gmail.com>
2024-08-21 00:39:53 -0500

    login con autentificacion

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { RegisterComponent } from './register/register.component';
+
+const routes: Routes = [
+  {path:'',redirectTo:'/iniciar-sesion', pathMatch:'full'},
+  {path:'inicio',component:DashboardComponent},
+  {path:'iniciar-sesion',component:LoginComponent},
+  {path:'register',component:RegisterComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
